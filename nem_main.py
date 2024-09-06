fe18d2ee7fa1e84acdd0578fd51bbfb5aca9a9a1
Skye R <skyerengr@gmail.com>
2024-09-05 17:36:38 -0700

    github cv-addition branch fixes.

--- nem_main.py
+++ nem_main.py
@@ -1,3 +1,13 @@
 import sys
 import math
 
+"""
+GIT STUFF:
+To push changes to git/github:
+0) git pull                             (Before working to update to current branch)
+1) git add .                            (adds all files to commit list)
+2) git commit -m "<message>"
+2.5) git branch -vv                     (double check it is going to the right branch)
+3) git push -u origin <branch>"
+
+"""
